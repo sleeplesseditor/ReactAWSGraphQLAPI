6374f51ee99f79c26b9f8f67301e82a067fec40d
sleeplesseditor <josh@bryson.me.uk>
2019-01-02 00:12:24 +0000

    GraphQL subscriptions setup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import logo from './awslogo.png';
 
 import { API, graphqlOperation } from 'aws-amplify';
 import { listRestaurants } from './graphql/queries';
+import { createRestaurant } from './graphql/mutations';
+import { onCreateRestaurant } from './graphql/subscriptions';
 
 class App extends Component {
-  state = { restaurants: [] }
+  state = {
+    name: '', 
+    description: '', 
+    restaurants: [] 
+  };
+
   async componentDidMount() {
     try {
       const apiData = await API.graphql(graphqlOperation(listRestaurants))
@@ -15,7 +22,50 @@ class App extends Component {
     } catch (err) {
       console.log('error: ', err)
     }
+
+    this.subscription = API.graphql(
+      graphqlOperation(onCreateRestaurant)
+    ).subscribe({
+      next: restaurantData => {
+        const restaurant = restaurantData.value.data.onCreateRestaurant
+        const restaurants = [
+          ...this.state.restaurants.filter(r => {
+            return (
+              r.name !== restaurant.name && r.description !== restaurant.description
+            )
+          }),
+          restaurant
+        ]
+        this.setState({ restaurants })
+      }
+    })
   };
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe()
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  createRestaurant = async () => {
+    const { name, description } = this.state
+    if (name === '' || description === '') return
+    try {
+      const restaurant = { name, description }
+      const restaurants = [...this.state.restaurants, restaurant]
+      this.setState({ 
+        restaurants, 
+        name: '', 
+        description: '' 
+      })
+      await API.graphql(graphqlOperation(createRestaurant, {input: restaurant}))
+      console.log('restaurant successfully created!')
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
 
   render() {
     return (
@@ -24,9 +74,33 @@ class App extends Component {
           <img src={logo} className="aws_logo" alt="logo" />
           <h1>Restaurant List API</h1>
         </div>
+
+        <div style={styles.inputContainer}>
+          <input
+            name='name'
+            placeholder='Restaurant Name'
+            onChange={this.onChange}
+            value={this.state.name}
+            style={styles.input}
+          />
+          <input
+            name='description'
+            placeholder='Restaurant Description'
+            onChange={this.onChange}
+            value={this.state.description}
+            style={styles.input}
+          />
+        </div>
+        <button
+          className="submit_button"
+          onClick={this.createRestaurant}
+        >
+          Create Restaurant
+        </button>
+
         {
           this.state.restaurants.map((rest, i) => (
-            <div style={styles.item}>
+            <div style={styles.item} key={rest.name}>
               <p style={styles.name}>{rest.name}</p>
               <p style={styles.description}>{rest.description}</p>
             </div>
@@ -38,14 +112,32 @@ class App extends Component {
 }
 
 const styles = {
+  inputContainer: {
+    margin: '0 auto', 
+    display: 'flex', 
+    flexDirection: 'column', 
+    width: 300
+  },
+  input: {
+    fontSize: 18,
+    border: 'none',
+    margin: 10,
+    height: 25,
+    backgroundColor: "#ddd",
+    padding: 8
+  },
   item: {
+    padding: 10,
     marginLeft: 200,
     marginRight: 200,
-    padding: 10,
     borderBottom: '2px solid #ddd'
   },
-  name: { fontSize: 22 },
-  description: { color: 'rgba(0, 0, 0, .45)' }
+  name: { 
+    fontSize: 22 
+  },
+  description: { 
+    color: 'rgba(0, 0, 0, .45)' 
+  }
 }
 
 export default App;
